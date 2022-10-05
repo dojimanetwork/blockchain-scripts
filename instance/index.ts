e259c074f62f56ec76c829a460a6fd85a691fffa
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-10-05 23:09:29 +0530

    create polka instance

---
+++ instance/index.ts
@@ -0,0 +1,9 @@
+import { ApiPromise, WsProvider } from '@polkadot/api'
+
+export default async function CreateInstance(): Promise<ApiPromise> {
+    const wsProvider = new WsProvider('wss://rpc.polkadot.io')
+    const api: ApiPromise = await ApiPromise.create({provider: wsProvider})
+    return api
+}
+
+
