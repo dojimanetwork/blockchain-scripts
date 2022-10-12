96f234cef3cdc7e9f3cce7aaa4fba9313ae3f71c
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-10-12 07:42:44 +0530

    dev: mod: wip fetch extinsics from block.

--- polka_go/go.mod
+++ polka_go/go.mod
@@ -1,3 +1,30 @@
 module github.com/dojimanetwork/polka-scripts/polka-go
 
 go 1.18
+
+require (
+	github.com/JFJun/go-substrate-crypto v1.0.1
+	github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.7
+)
+
+require (
+	github.com/ChainSafe/go-schnorrkel v1.0.0 // indirect
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
+	github.com/cosmos/go-bip39 v1.0.0 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/base58 v1.0.3 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.10.25 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/gtank/merlin v0.1.1 // indirect
+	github.com/gtank/ristretto255 v0.1.2 // indirect
+	github.com/mimoo/StrobeGo v0.0.0-20210601165009-122bf33a46e0 // indirect
+	github.com/pierrec/xxHash v0.1.5 // indirect
+	github.com/rs/cors v1.8.2 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/vedhavyas/go-subkey v1.0.3 // indirect
+	golang.org/x/crypto v0.0.0-20221005025214-4161e89ecf1b // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+)
--- polka_go/main.go
+++ polka_go/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"github.com/vedhavyas/go-subkey"
+	"github.com/vedhavyas/go-subkey/scale"
+
+	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
+)
 
 func main() {
-	fmt.Println("starting")
+	//client, err := dotc.Connect("wss://westend-rpc.polkadot.io")
+	api, err := gsrpc.NewSubstrateAPI("wss://westend-rpc.polkadot.io")
+
+	if err != nil {
+		fmt.Errorf("error in connecting %w", err)
+	}
+
+	//rpcClient := dotRpc.NewChain(client)
+
+	//if err != nil {
+	//	fmt.Errorf("error in rpc connection %w", err)
+	//}
+
+	blockHash, err := api.RPC.Chain.GetBlockHash(12782886)
+
+	if err != nil {
+		fmt.Errorf("error in rpc connection %w", err)
+	}
+
+	blockInfo, err := api.RPC.Chain.GetBlock(blockHash)
+
+	for _, extrinsic := range blockInfo.Block.Extrinsics {
+		if extrinsic.Method.CallIndex.SectionIndex == 16 && extrinsic.Method.CallIndex.MethodIndex == 0 {
+			fmt.Printf("extrinsic %v\n\n\n\n", extrinsic)
+			//fmt.Printf("version %d\n", extrinsic.Type())
+			sender, _ := subkey.SS58Address(extrinsic.Signature.Signer.AsID[:], uint8(42))
+			fmt.Println("BXL: DOTChain pk bytes: ", sender)
+
+			_ = scale.NewDecoder(bytes.NewReader(extrinsic.Method.Args))
+			fmt.Println("BXL: ext.Method.Args: ", extrinsic.Method.Args)
+		}
+	}
+
 }
