8c3ad42445b7da0bff94a4abaacaeaeaf30bb805
bhaagiKenpachi <bhagath.reddy@dojima.network>
2022-10-15 14:22:10 +0530

    dev: mod: fetch latest block custom code.

--- polka_go/main.go
+++ polka_go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
 	irpc "github.com/itering/substrate-api-rpc/rpc"
@@ -15,7 +17,15 @@ const (
 )
 
 type Client struct {
+	latestBlock  *irpc.JsonRpcResult
 	dotRpcClient *gsrpc.SubstrateAPI
+}
+
+//system_syncState
+type DotSyncState struct {
+	StartingBlock int `json:"startingBlock"`
+	CurrentBlock  int `json:"currentBlock"`
+	HighestBlock  int `json:"highestBlock"`
 }
 
 func main() {
@@ -44,13 +54,17 @@ func main() {
 
 	blockResult := &irpc.JsonRpcResult{}
 
-	err = iws.SendWsRequest(nil, blockResult, irpc.ChainGetBlockHash(1, 1))
+	query, err := json.Marshal(irpc.Param{Id: 2, Params: []string{}, JsonRpc: "2.0", Method: "system_syncState"})
+	if err != nil {
+		_ = fmt.Errorf("query error %w", err)
+	}
+	err = iws.SendWsRequest(nil, blockResult, query)
 	if err != nil {
 
 	}
-	fmt.Println(blockResult)
-	blockHash := blockResult.Result
-	fmt.Println(blockHash)
+	cli.latestBlock = blockResult
+
+	fmt.Println(cli.ToSyncState().CurrentBlock)
 
 }
 
@@ -58,4 +72,37 @@ func (c *Client) GetHeight() (int64, error) {
 	BlockInfo, err := c.dotRpcClient.RPC.Chain.GetBlockLatest()
 	// TODO: verify conversion of uint32 dot block number to int64
 	return int64(BlockInfo.Block.Header.Number), err
+}
+
+func (c *Client) ToSyncState() *DotSyncState {
+
+	if c.checkErr() != nil {
+		return nil
+	}
+
+	if (c.latestBlock).Result == nil {
+		return nil
+	}
+
+	result := (c.latestBlock).Result.(map[string]interface{})
+
+	if len(result) == 0 {
+		return nil
+	}
+	v := &DotSyncState{}
+	marshal, err := json.Marshal(result)
+	fmt.Println(result)
+	if err != nil {
+		return nil
+	}
+	_ = json.Unmarshal(marshal, v)
+
+	return v
+}
+
+func (p *Client) checkErr() error {
+	if p.latestBlock.Error != nil {
+		return errors.New(p.latestBlock.Error.Message)
+	}
+	return nil
 }
