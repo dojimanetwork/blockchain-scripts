61fcc88e564a62b1e3de095af84f4c6ef520e9dc
bhaagiKenpachi <bhagath.reddy@dojima.network>
2023-08-31 13:16:48 +0530

    dev:mod: whitelist address tx

--- adkg_contracts/scripts/configure_whitelist.js
+++ adkg_contracts/scripts/configure_whitelist.js
@@ -3,32 +3,36 @@ const hre = require("hardhat");
 
 const NodeList = hre.artifacts.readArtifact('contracts/NodeList.sol:NodeList')
 const whitelistedAccounts = require('../whiteList.js')
-
+const contracts = require("../contract.json")
 
 function tx(result, call) {
-    const logs = result.logs.length > 0 ? result.logs[0] : { address: null, event: null }
+    // const logs = result.logs.length > 0 ? result.logs[0] : { address: null, event: null }
 
     console.log()
     console.log(`   Calling ${call}`)
     console.log('   ------------------------')
-    console.log(`   > transaction hash: ${result.tx}`)
-    console.log(`   > contract address: ${logs.address}`)
-    console.log(`   > gas used: ${result.receipt.gasUsed}`)
-    console.log(`   > event: ${logs.event}`)
+    console.log(`   > transaction hash: ${result.hash}`)
+    // console.log(`   > contract address: ${logs.address}`)
+    // console.log(`   > gas used: ${result.receipt.gasUsed}`)
+    // console.log(`   > event: ${logs.event}`)
     console.log()
 }
 
 async function main() {
     const signers = await hre.ethers.getSigners()
-    const NodeListInstance = await NodeList
-    tx(await NodeListInstance.updateEpoch(1, 5, 3, 1, [], 0, 2), 'Updated epoch')
+
+    const nlAddr = contracts["nodelist_address"]
+    const NL = await hre.ethers.getContractFactory("NodeList")
+    const nl = await NL.attach(nlAddr)
+
+    tx(await nl.updateEpoch(1, 5, 3, 1, [], 0, 2), 'Updated epoch')
     for (let i = 0; i < whitelistedAccounts.length; i++) {
         const acc = whitelistedAccounts[i]
-        tx(await NodeListInstance.updateWhitelist(1, acc, true, { from: signers[0], gas: '100000' }), `adding ${acc} to whitelist`)
+        tx(await nl.updateWhitelist(1, acc, true, { from: signers[0], gas: '100000' }), `adding ${acc} to whitelist`)
     }
 
     for (var i = 0; i < whitelistedAccounts.length; i++) {
-        var res = await NodeListInstance.IsWhitelisted(1, whitelistedAccounts[i])
+        var res = await nl.IsWhitelisted(1, whitelistedAccounts[i])
         console.log('should be whitelisted, isWhitelisted: ', res)
     }
 }
